6a1ccdb15e17d1c24b160b38a97fa4923db7d100
rajeev.singh <rajeev.singh@gradeup.co>
2018-08-23 11:21:20 +0530

    checking

--- flutter_intro/lib/main.dart
+++ flutter_intro/lib/main.dart
@@ -19,8 +19,9 @@ class MyApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
       
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+
     );
   }
 }
