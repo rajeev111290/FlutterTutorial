7e318e2aaf6d68a6f4de779300fb57af9b33e890
rajeev.singh <rajeev.singh@gradeup.co>
2018-08-28 12:36:20 +0530

    material theme

--- flutter_intro/lib/main.dart
+++ flutter_intro/lib/main.dart
@@ -15,8 +15,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.green
       ),
 //      home: new MyHomePage(title: 'Flutter Tutorial'),
       initialRoute: '/',
